d419ab969386a141b6fa57fa9b29a2c398a5425e
gregoire cade <gregoirecade.fr@gmail.com>
2020-11-12 17:01:34 +0000

    Destroy Dose

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,16 +8,21 @@ class DosesController < ApplicationController
     @dose = Dose.new(strong_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
-      if @dose.save
-        redirect_to cocktail_path(@cocktail)
-      else
-        render :new
-      end
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
-    private
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose)
+  end
+  private
 
   def strong_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
